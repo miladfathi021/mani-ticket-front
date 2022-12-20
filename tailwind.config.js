25636c25bfbd53b125b039ca17bf3e1332d102c5
Milad <miladfathi021@gmail.com>
2022-12-21 00:44:27 +0330

    Added tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: {
+          1: "#27296d",
+          2: "#5e63b6",
+          3: "#a393eb",
+          4: "#f5c7f7",
+          5: "#A2A4A8",
+          6: "#222438",
+        },
+      },
+    },
   },
   plugins: [],
 };
