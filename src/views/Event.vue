7f66079e0bd81d0551824c538dcdd089fb70ce27
Milad <miladfathi021@gmail.com>
2022-12-24 01:22:07 +0330

    Created event page

---
+++ src/views/Event.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="events flex items-center h-full">
+    <div class="w-1/3 h-full mr-2 px-2 py-4 bg-theme-1">e</div>
+    <div class="w-2/3 h-full ml-2 px-2 py-4">d</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Event",
+};
+</script>
+
+<style lang="scss" scoped></style>
