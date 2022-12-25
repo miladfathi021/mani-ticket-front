dc249f83d91d1aed73ae4eec019b41b6cf07f506
Milad <miladfathi021@gmail.com>
2022-12-26 00:50:41 +0330

    Created event page

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,14 +1,113 @@
 <template>
-  <div class="events flex items-center h-full">
-    <div class="w-1/3 h-full mr-2 px-2 py-4 bg-theme-1">e</div>
-    <div class="w-2/3 h-full ml-2 px-2 py-4">d</div>
+  <div v-if="eventDetails" class="event">
+    <div class="event--left-side">
+      <div class="event--left-side__box">
+        <div class="event--left-side__img">
+          <img :src="eventDetails.image" :alt="eventDetails.name" />
+        </div>
+        <span class="block text-2xl font-bold text-theme-1 mt-3">
+          {{ eventDetails.name }}
+        </span>
+        <div class="flex items-center mt-4">
+          <vue-feather type="user" size="20px" stroke="#A2A4A8" />
+          <span class="block ml-2 text-lg font-medium text-theme-7">
+            {{ eventDetails.artist.name }}
+          </span>
+        </div>
+        <div class="flex items-center mt-2">
+          <span class="block ml-2 text-sm font-normal text-theme-7">
+            {{ eventDetails.description }}
+          </span>
+        </div>
+        <div class="flex items-center mt-5">
+          <vue-feather type="map-pin" size="20px" stroke="#A2A4A8" />
+          <span class="block ml-2 text-lg font-medium text-theme-7">
+            {{ eventDetails.complex.name }}
+          </span>
+        </div>
+        <div class="flex items-center mt-2">
+          <span class="block ml-2 text-sm font-normal text-theme-7">
+            {{ eventDetails.complex.address }}
+          </span>
+        </div>
+        <div class="flex items-center mt-5">
+          <vue-feather type="calendar" size="20px" stroke="#A2A4A8" />
+          <span class="block ml-2 text-lg font-medium text-theme-7">
+            sales start date
+          </span>
+        </div>
+        <div class="flex items-center mt-2">
+          <span class="block ml-2 text-sm font-normal text-theme-7">
+            {{ getDate(eventDetails.date_start) }}
+          </span>
+          <span class="block ml-2 text-sm font-normal text-theme-7">
+            {{ getTime(eventDetails.time_start) }}
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="event--right-side">
+      <div class="event--right-side__box">
+        <Halls :items="eventDetails.halls" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import dateTimeMixin from "@/mixins/DateTimeMixin";
+import Halls from "@/components/Event/Halls";
+
 export default {
   name: "Event",
+  components: { Halls },
+  mixins: [dateTimeMixin],
+  created() {
+    this.$store.dispatch("event/getEventDetails", this.$route.params.id);
+  },
+  computed: {
+    ...mapGetters({
+      eventDetails: "event/eventDetails",
+    }),
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.event {
+  @apply flex items-center h-full;
+
+  &--left-side {
+    @apply w-1/3 h-full pl-20 py-10 bg-theme-1 overflow-hidden;
+
+    &__box {
+      @apply h-full rounded-l-2xl p-4 bg-gray-50;
+      box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    &__img {
+      @apply rounded-2xl overflow-hidden;
+      width: 100%;
+      min-height: 300px;
+      max-height: 300px;
+
+      & img {
+        width: 100%;
+        min-height: 300px;
+        max-height: 300px;
+        object-fit: cover;
+      }
+    }
+  }
+
+  &--right-side {
+    @apply w-2/3 h-full pr-20 py-10 bg-gray-100 overflow-hidden;
+
+    &__box {
+      @apply bg-white h-full rounded-r-2xl p-4;
+      box-shadow: 0 0 15px 5px rgba(0, 0, 0, 0.1);
+    }
+  }
+}
+</style>
